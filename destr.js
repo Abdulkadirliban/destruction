56608c6b4b6b6c3a62330b9d6a684a18f31d07e1
abdul <abdulkadirliban5631@gmail.com>
2022-03-12 08:54:40 +0300

    added des

--- destr.js
+++ destr.js
@@ -1,4 +1,15 @@
+//------Array destruction ------
+
 let std={id:585,name:"Abdul",age:23,phone:617055631};
 let{id,name,age,phone}=std;
 console.log(std);
 console.log(`ID:${id} Name:${name} Age ${age} Phone ${phone}`);
+
+//------Object destruction ------
+const std2={
+    username:"farah",
+    ID:567,
+    Age:23
+};
+const{username,ID,Age}=std2
+console.log(`Username: ${username} Id: ${ID} AGE: ${Age}`)
